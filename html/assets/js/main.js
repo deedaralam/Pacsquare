9b17c05566ca24e8479bffc5f88d9711c6cdef94
Deedar Alam <deedaralam@gmail.com>
2015-09-22 15:21:20 +0500

    First Commit

---
+++ html/assets/js/main.js
@@ -0,0 +1,47 @@
+
+
+(function(){
+  "use strict";
+
+
+$(function ($) {
+
+
+
+    $("#thumb-carousel").owlCarousel({
+        navigation : false,
+        pagination: false,
+        responsive: true,
+        items: 2,
+        touchDrag: true,
+        mouseDrag: true,
+        itemsDesktop: [3000,8],
+        itemsDesktopSmall: [1440,2],
+        itemsTablet:[800,2],
+        itemsTabletSmall: [600,2],
+        itemsMobile: [360,1],
+        autoPlay: true
+      });
+
+
+   
+
+
+
+
+
+
+});
+// $(function ($)  : ends
+
+})();
+//  JSHint wrapper $(function ($)  : ends
+
+
+
+
+
+
+
+  
+
