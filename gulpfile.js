37fe8adb7149d81107ae4d7f9aaa5c746268d8cb
Deedar Alam <deedaralam@gmail.com>
2015-09-22 17:08:33 +0500

    Added CoffeeScript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util');
+	gutil = require('gulp-util'),
+	coffee = require('gulp-coffee');
+	
+var coffeeSources = ['components/coffee/tagline.coffee'];
 
-gulp.task('log', function() {
-	gutil.log('Pacsquare Website are awesome');
+gulp.task('coffee', function(){
+	gulp.src(coffeeSources)
+		.pipe(coffee({bare:true}))
+		.pipe(gulp.dest('components/scripts'))
 });
